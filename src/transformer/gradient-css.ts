902ab92229f3a42234dc82bd001abf6152a9f75b
Doug Perez <douglas.perez@rackspace.com>
2023-07-02 17:58:56 -0700

    save work

--- src/transformer/gradient-css.ts
+++ src/transformer/gradient-css.ts
@@ -6,13 +6,17 @@ type TokenGradient = {
   position: string
   angle: number
 }
+const hasSpaceInName = (string: string) => /\s/g.test(string)
+
 
 export const gradientCss: StyleDictionary.Transform = {
   type: `value`,
   transitive: true,
   matcher: isGradient,
-  transformer: ({ value }: { value: TokenGradient }) =>
-    `${value.angle} ${value.color} ${value.position}`
-  // transformer: (token: StyleDictionary.TransformedToken) => token.value.map((string: string) => hasSpaceInName(string) ? `'${string}'` : string).join(", ")
-
+  // transformer: ({ value }: { value: TokenGradient }) =>
+  //   `${value.angle} ${value.color} ${value.position}`
+  transformer: (token: StyleDictionary.TransformedToken) => {
+    console.log('token!', token)
+   return (token.value.map((string: string) => hasSpaceInName(string) ? `'${string}'` : string).join(", "))
+  } 
 }
