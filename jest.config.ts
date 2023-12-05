ed8511934efdcf42595b24b320a6eb24168b30d9
jorenbroekema <joren.broekema@gmail.com>
2023-12-05 18:53:52 +0100

    chore: apply fixes for jest + ESM, prettier sync

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,18 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   reporters: ['default'],
+  // https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
 
 export default config;
