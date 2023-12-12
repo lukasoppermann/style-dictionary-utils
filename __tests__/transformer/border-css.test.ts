8e693c4b2f6f0197bce6da8f57671c9aca4a3d99
Lukas Oppermann <lukasoppermann@github.com>
2023-12-12 16:21:39 +0100

    fix lint

--- __tests__/transformer/border-css.test.ts
+++ __tests__/transformer/border-css.test.ts
@@ -1,25 +1,26 @@
 import StyleDictionary from 'style-dictionary';
 import { borderCss } from '../../src/transformer/border-css';
+import { getMockToken } from '../../src/testUtilities/getMockToken';
+
 
 describe('Transformer: border', () => {
-  const items = [{
+  const items = [getMockToken({
     value: {
       color: '#334455',
       width: '1px',
       style: 'dashed'
     },
     $type: 'border',
-  }, {
+  }), getMockToken({
     value: {
       color: '#33445566',
       width: '5px',
       style: 'solid'
     },
     $type: 'border',
-  }];
+  })];
 
   it('transforms `border` tokens', () => {
-    // @ts-ignore: wrong type in test
     expect(items.map(item => borderCss.transformer(item as StyleDictionary.TransformedToken, {}))).toStrictEqual([
       "1px dashed #334455",
       "5px solid #33445566",
