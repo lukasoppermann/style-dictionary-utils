c01cfd94eb99a454c40ff2412833dd6b7c3bd5af
Jimmy Morris <jimmy.e.morris@gmail.com>
2023-12-12 16:04:02 -0500

    fix: shadowCSS references returning `0 0 0 0 undefined` when value is a string
    When a shadow value is a reference, sometimes the value will already be transformed to a `string` vs a `type TokenShadow`. This fix is to add that check for a string and return the value is so.

--- src/transformer/shadow-css.ts
+++ src/transformer/shadow-css.ts
@@ -15,6 +15,10 @@ export const shadowCss: StyleDictionary.Transform = {
   type: `value`,
   transitive: true,
   matcher: isShadow,
-  transformer: ({ value }: { value: TokenShadow }) =>
-    `${value.offsetX || 0} ${value.offsetY || 0} ${value.blur || 0} ${value.spread || 0} ${value.color}`
+  transformer: ({ value }: { value: string | TokenShadow }) =>
+  typeof value === 'string'
+    ? value
+    : `${value.offsetX || 0} ${value.offsetY || 0} ${value.blur || 0} ${value.spread || 0} ${
+        value.color
+      }`,
 }
