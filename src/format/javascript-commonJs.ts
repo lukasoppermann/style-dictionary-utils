399ae67fb820c6a481b01bf7261bc511dbcaeb86
Lukas Oppermann <lukasoppermann@github.com>
2024-03-10 21:14:06 +0100

    fix commonjs tests

--- src/format/javascript-commonJs.ts
+++ src/format/javascript-commonJs.ts
@@ -6,11 +6,11 @@ import { jsonToNestedValue } from '../utilities/jsonToNestedValue.js'
 
 const { fileHeader } = StyleDictionary.formatHelpers
 
-export const javascriptCommonJs: Format['formatter'] = ({ dictionary, file, options, platform = {} }) => {
+export const javascriptCommonJs: Format['formatter'] = async ({ dictionary, file, options, platform = {} }) => {
   const { prefix } = platform
   const tokens = prefix ? { [prefix]: dictionary.tokens } : dictionary.tokens
   //
-  const output = fileHeader({ file }) +
+  const output = await fileHeader({ file }) +
     `exports.default = ${JSON.stringify(jsonToNestedValue(tokens), null, 2)}\n`
   // return prettified
   return syncPrettier.format(output, { parser: 'typescript', printWidth: 500, ...options?.prettier })
