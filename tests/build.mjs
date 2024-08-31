5ebfc37ac8e7d2ace4e7d13c45acee6068e46e35
Lukas Oppermann <lukasoppermann@github.com>
2024-08-31 09:05:06 +0200

    fixing formats

--- tests/build.mjs
+++ tests/build.mjs
@@ -1,7 +1,6 @@
 import { StyleDictionary as SD } from "../dist/index.js";
 
 const StyleDictionary = new SD();
-
 const extendSd = await StyleDictionary.extend({
   source: ["./tests/tokens/*.json5"],
   platforms: {
@@ -40,6 +39,48 @@ const extendSd = await StyleDictionary.extend({
         {
           format: "css/advanced",
           destination: "advanced-variables.css",
+          options: {
+            outputReferences: true,
+          }
+        },
+      ],
+    },
+    commonJs: {
+      prefix: "PREFIX",
+      buildPath: "./tests/dist/js/",
+      transformGroup: "js",
+      files: [
+        {
+          format: "javascript/commonJs",
+          destination: "commonJs.js",
+          options: {
+            outputReferences: true,
+          }
+        },
+      ],
+    },
+    esm: {
+      prefix: "PREFIX",
+      buildPath: "./tests/dist/js/",
+      transformGroup: "js",
+      files: [
+        {
+          format: "javascript/esm",
+          destination: "esm.mjs",
+          options: {
+            outputReferences: true,
+          }
+        },
+      ],
+    },
+    esmDeclaration: {
+      prefix: "PREFIX",
+      buildPath: "./tests/dist/js/",
+      transformGroup: "js",
+      files: [
+        {
+          format: "typescript/esm-declarations",
+          destination: "esm.d.ts",
           options: {
             outputReferences: true,
           }
