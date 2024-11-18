7fa3415e21856ec0999365ba9fee879226f2fcf7
Lukas Oppermann <lukasoppermann@github.com>
2024-11-18 16:55:50 +0100

    getValue function

--- src/filter/isClamp.ts
+++ src/filter/isClamp.ts
@@ -1,12 +1,18 @@
 import {TransformedToken} from 'style-dictionary/types'
+import {getValue} from '../utilities/getValue'
 /**
  * @name isClamp
  * @type filter
  * @description only returns tokens of type `shadow`
  */
-export const isClamp = (token: TransformedToken): boolean =>
-  (token?.$type === 'clamp' || token?.type === 'clamp') &&
-  typeof token.value === 'object' &&
-  'min' in token.value &&
-  'ideal' in token.value &&
-  'max' in token.value
+export const isClamp = (token: TransformedToken): boolean => {
+  const tokenValue = getValue(token)
+  return (
+    (token?.$type === 'clamp' || token?.type === 'clamp') &&
+    tokenValue !== null &&
+    typeof tokenValue === 'object' &&
+    'min' in tokenValue &&
+    'ideal' in tokenValue &&
+    'max' in tokenValue
+  )
+}
